7d81c9c69fbc1452ab59430614b5dc9466b5b3fd
Kolado Sidibe <ksidibe@yahoo.com>
2023-02-09 07:17:00 -0500

    Add query scope for filtering by tenant_id

--- src/Models/GenericParticipant.php
+++ src/Models/GenericParticipant.php
@@ -3,8 +3,16 @@
 namespace Andaletech\Inbox\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Builder;
 use Andaletech\Inbox\Traits\DateTimeCastTrait;
 
+/**
+ * Model for a generic participant.
+ *
+ * @param string $display_name
+ * @param string $recipient_id
+ * @param string $recipient_id_prefix
+ */
 class GenericParticipant extends Model /* implements IMessageRecipient */
 {
   use DateTimeCastTrait;
@@ -39,6 +47,54 @@ class GenericParticipant extends Model /* implements IMessageRecipient */
 
     return  $name;
   }
+
+  #region query scopes
+
+  public function scopeForTenant(Builder $query, int $tenantId)
+  {
+    if ($this->isInboxMultiTenant && $this->tenantIdColumn) {
+      return $query->where($this->tenantIdColumn, $tenantId);
+    }
+
+    return $query;
+  }
+
+  #endregion query scopes
+
+  #region IMessageRecipient
+
+  public function getRecipientDisplayNameAttribute()
+  {
+    return $this->display_name;
+  }
+
+  public function getRecipientIdAttribute()
+  {
+    return $this->recipient_id_prefix . $this->id;
+  }
+
+  /**
+   * @inheritDoc
+   */
+  public function getRecipientIdPrefixAttribute()
+  {
+    return 'generic_';
+  }
+  #endregion IMessageRecipient
+
+  #region static/utility methods
+
+  public static function isTenant()
+  {
+    return config('andale-inbox.tenancy.multi_tenant');
+  }
+
+  public static function getTenantColumnId()
+  {
+    return config('andale-inbox.tenancy.tenant_id_column', 'tenant_id');
+  }
+
+  #endregion static/utility methods
 
   #region override parent methods.
 
